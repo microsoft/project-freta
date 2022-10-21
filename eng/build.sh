9e270e31a1608c4fc84912e046bebaa5e52c57de
bmc-msft <41130664+bmc-msft@users.noreply.github.com>
2022-10-21 10:10:31 -0400

    use build script to enable developers to run the builds locally (#9)

---
+++ eng/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+set -exu
+
+cargo fmt -- --check
+cargo clippy --all-targets --all-features --locked -- -D warnings -D clippy::pedantic -A clippy::missing_errors_doc
+cargo test --release --locked
+cargo build --release --locked
+cargo build --examples --all-features --release --locked
